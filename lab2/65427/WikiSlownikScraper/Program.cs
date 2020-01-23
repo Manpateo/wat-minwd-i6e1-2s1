96f925a446454aadd7f9394f0545f1d29a6489af
Sebastian <seg96@interia.pl>
2020-01-23 08:02:32 +0100

    poprawa zadania 2

---
+++ lab2/65427/WikiSlownikScraper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using HtmlAgilityPack;
+
+namespace WikiSlownikScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HtmlReader.ReadSite("https://pl.wiktionary.org/wiki/Indeks:Angielski_-_Ssaki?fbclid=IwAR3RVwyFUYg8l86yGkg4OPu9MRZr4S3Vi8bc5SxYVN-2jeyvUvSgHXnpcyI");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab2/65427/readme.md
@@ -0,0 +1,11 @@
+Autor*
+Sebastian Głowacz
+Opis*
+Scraper pobierający i wyświetlający w formacie json dane np. ssaków ze strony Wikisłownik.
+Program napisano w C#, w dotnet.
+Instrukcja*
+1.W klasie Program, w odpowiednim miejscu wpisać adres url strony, którą chcemy scrapować
+1.Otworzyć folder repozytorium kodu (WikiSlownikScraper) w konsoli
+2.Wpisać komendę "dotnet run"
+3.Podziwiać wynik scrapowania w konsoli
+
